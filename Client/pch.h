ca04d14c7cb357cb1a37e13136d9edfa38aded83
Nabin Kim <knabinn97@gmail.com>
2021-06-03 23:38:38 +0900

    엔진 라이브러리와 클라이언트 연결

--- Client/pch.h
+++ Client/pch.h
@@ -1,4 +1,9 @@
-#pragma once
+﻿#pragma once
+
+// 전처리 시점에서 라이브러리 사용하겠다고 선언한다. (링커 - 추가 종속성과 동일하게 동작)
+#pragma comment(lib, "Engine.lib")
+
+#include "EnginePch.h"
 
 #include <vector>
 #include <memory>
--- Engine/EnginePch.cpp
+++ Engine/EnginePch.cpp
@@ -1,2 +1,6 @@
 #include "pch.h"
 #include "EnginePch.h"
+
+void HelloEngine()
+{
+}
